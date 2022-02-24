1077bddc16890afe93cfb203cb90ffe1c9fb880b
Arnav Singhal <singhal.arnav@gmail.com>
2022-02-24 22:56:55 +0530

    Initial Commit

---
+++ CryptoWatcher/CryptoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWatcherApp.swift
+//  CryptoWatcher
+//
+//  Created by Arnav Singhal on 24/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
